483c8665504154dc88d61909220cd707eb25a535
Shivbhan Kushwaha <108090221+ShivBhanKushWaha@users.noreply.github.com>
2023-05-19 20:37:53 +0530

    Update work.js

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,18 +1,18 @@
 export const WorkData = [
     {
-        company:"Company 1",
-        designation:"Software Engineer",
-        dateJoining:"02-02-2024",
+        company:"Random Learning",
+        designation:"Full Stack Developer",
+        dateJoining:"02-03-2022",
         dateEnd:"Present",
         companyLogo:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg",
         work:"Google LLC  is an American multinational technology company that focuses on search engine technology, online advertising, computer software, e-commerce, artificial intelligence, and consumer electronics."
     },
     {
-        company:"Company 2",
-        designation:"Software Engineer",
-        dateJoining:"02-02-2024",
+        company:"Random Learning",
+        designation:"DSA",
+        dateJoining:"02-07-2020",
         dateEnd:"Present",
-        companyLogo:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg",
+        companyLogo:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg",
         work:"Facebook is a website which allows users, who sign-up for free profiles, to connect with friends, work colleagues or people they don't know, online. as well as their own thoughts and opinions with however many people they like."
     },
 ];
